4a86f8ced6455716433aa71b3e178c80c1adb174
murra394 <cecile.m.murray@census.gov>
2021-03-19 10:13:30 -0400

    fix static files settings

--- dcdl/example_settings.py
+++ dcdl/example_settings.py
@@ -149,7 +149,7 @@ LOGIN_REDIRECT_URL = '/EntryApp/'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/deepzoom/js/vendor/openseadragon/images/')
 
 MEDIA_URL = '/images/' # this is the thing to change
 MEDIA_ROOT = '/data/data/git/images'
