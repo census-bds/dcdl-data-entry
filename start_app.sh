1298fe989b27d6e27ab60603bff49ad860f2d67c
murra394 <cecile.m.murray@census.gov>
2021-09-02 12:59:57 -0400

    change start_app.sh for prod configuration

--- start_app.sh
+++ start_app.sh
@@ -3,6 +3,6 @@
 # activate the dcdl conda environment
 source /apps/user/${USER}/miniconda3/bin/activate /apps/user/${USER}/conda_envs/dcdl
 
-# move to app dir and start django at 7000
+# move to app dir and start django at 8000 
 cd /apps/django/dcdl_test
-python manage.py runserver 7000
+python manage.py runserver 8000
