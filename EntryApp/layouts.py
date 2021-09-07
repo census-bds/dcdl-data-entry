066fe14df400c5aeac392f2460386492ed44703b
murra394 <cecile.m.murray@census.gov>
2021-09-07 14:47:23 -0400

    fix 1980 layout; begin 1990

--- EntryApp/layouts.py
+++ EntryApp/layouts.py
@@ -30,8 +30,8 @@ FORM_1960 = Layout(
         Field(InlineRadios('birth_year')),
         Field(InlineRadios('marital_status')),
         Field('street_name'),
-        Field('house_no'),
-        Field('apt_no'),
+        Field('house_no', css_class='numberinput-small'),
+        Field('apt_no', css_class='numberinput-small'),
         css_id = 'form-row-div',
         css_class='form-inline form-inline-nowrap',
     )
@@ -69,60 +69,55 @@ FORM_1970 = Layout(
 )
 
 FORM_1980 = Layout(
-    Div(
-        Div('col_num', css_class='row col-lg-3 col-md-3 col-sm-3'),
-        Div('last_name', css_class='row col-lg-11 col-md-11 col-sm-11'),
+   Div(
+        Field('col_num'),
+        Field('last_name'),
         Div(
-            Div('first_name', css_class='col-lg-8 col-md-8 col-sm-8'),
-            Div('middle_init', css_class='col-lg-4 col-md-4 col-sm-4'),
-            css_class='row'
+            Field('first_name'),
+            Field('middle_init', css_class='textinput-small'),
+            css_id='form-1980-first-name-middle-init'
         ),
-        Div('sex', css_class='row'),
-        Div(Div('race_1980', css_id='race_1980'), css_class='row'),
-        Div(
-            'age',
-            'exact_birth_month',
-            'birth_quarter',
-            css_class='col-lg-4 col-md-4 col-sm-4'
-        ),
-        Div(
-            Div('exact_birth_year', css_class='row'),
-            Div('birth_year_thousands', css_class='col-lg-2 col-md-2 col-sm-2'),
-            Div('birth_year_hundreds', css_class='col-lg-2 col-md-2 col-sm-2'),
-            Div('birth_year_tens', css_class='col-lg-2 col-md-2 col-sm-2'),
-            Div('birth_year_ones', css_class='col-lg-2 col-md-2 col-sm-2'),
-            css_class='col-lg-8 col-md-8 col-sm-8'
-        ),
-        Div(
-            Div('block', css_class='col-lg-4 col-md-4 col-sm-4'),
-            Div('serial_no', css_class='col-lg-4 col-md-4 col-sm-4'),
-            Div('total_persons', css_class='col-lg-4 col-md-4 col-sm-4'),
-            css_class='row'
-        ),
+        Div(InlineRadios('sex'), css_id='form-1980-sex-radios'),
+        Div('race_1980', css_id='form-1980-race-radios'),
         Div(
             Div(
-                Div('block_1', css_class='col-lg-4 col-md-4 col-sm-4'),
-                Div('block_2', css_class='col-lg-4 col-md-4 col-sm-4'),
-                Div('block_3', css_class='col-lg-4 col-md-4 col-sm-4'),
-                css_class="col-lg-4 col-md-4 col-sm-4"
+                Field('age'),
+                Field('exact_birth_month'),
+                Div('birth_quarter', css_id='race-1980-vertical-radios'),
             ),
+            Field('exact_birth_year', css_class='row'),
             Div(
-                Div('serial_no_1', css_class='col-lg-3 col-md-3 col-sm-3'),
-                Div('serial_no_2', css_class='col-lg-3 col-md-3 col-sm-3'),
-                Div('serial_no_3', css_class='col-lg-3 col-md-3 col-sm-3'),
-                Div('serial_no_4', css_class='col-lg-3 col-md-3 col-sm-3'),
-                css_class='col-lg-4 col-md-4 col-sm-4'
-            ),
-            Div(
-                Div('total_persons_hundreds', css_class='col-lg-4 col-md-4 col-sm-4'),
-                Div('total_persons_tens', css_class='col-lg-4 col-md-4 col-sm-4'),
-                Div('total_persons_ones', css_class='col-lg-4 col-md-4 col-sm-4'),
-                css_class='col-lg-4 col-md-4 col-sm-4'
-            ),
+                Div(InlineRadios('birth_year_thousands'), css_class='col-lg-3 col-md-3 col-sm-3'),
+                Div(InlineRadios('birth_year_hundreds'), css_class='col-lg-3 col-md-3 col-sm-3'),
+                Div(InlineRadios('birth_year_tens'), css_class='col-lg-3 col-md-3 col-sm-3'),
+                Div(InlineRadios('birth_year_ones'), css_class='col-lg-3 col-md-3 col-sm-3'),
+                css_class='row'
+            )
+        ),
+        Field('block'),
+        Div(
+            Div('block_1', css_class='col-lg-4 col-md-4 col-sm-4'),
+            Div('block_2', css_class='col-lg-4 col-md-4 col-sm-4'),
+            Div('block_3', css_class='col-lg-4 col-md-4 col-sm-4'),
+            css_class="row"
+        ),
+        Field('serial_no'),
+        Div(
+            Div('serial_no_1', css_class='col-lg-3 col-md-3 col-sm-3'),
+            Div('serial_no_2', css_class='col-lg-3 col-md-3 col-sm-3'),
+            Div('serial_no_3', css_class='col-lg-3 col-md-3 col-sm-3'),
+            Div('serial_no_4', css_class='col-lg-3 col-md-3 col-sm-3'),
             css_class='row'
         ),
-    css_class='table-bordered col-lg-4 col-md-4 col-sm-4'
-    )
+        Field('total_persons'),
+        Div(
+            Div('total_persons_hundreds', css_class='col-lg-4 col-md-4 col-sm-4'),
+            Div('total_persons_tens', css_class='col-lg-4 col-md-4 col-sm-4'),
+            Div('total_persons_ones', css_class='col-lg-4 col-md-4 col-sm-4'),
+            css_class='row'
+        ),
+    css_class='form-inline form-column'
+    )    
 )
 
 FORM_1990 = Layout(
@@ -221,74 +216,18 @@ FORM_DICT = {
 
 CRISPY_FORM_1960 = FORM_1960
 
-CRISPY_FORM_1970 = Layout(
-    Div(
-        Div(
-            Field('last_name'),
-            Div(
-                Field('first_name'),
-                Field('middle_init', css_class='textinput-small'),
-                css_id="form-1970-first-name-middle-init"
-            ),
-            css_id='form-1970-names-div'
-        ),
-        Field('relp_1970'),
-        Field('sex'),
-        Field(InlineRadios('race_1970')),
-        Div(
-            Field('exact_birth_month'),
-            Field('exact_birth_year'),
-            Field('age'),
-            css_id="form-1970-age-block"
-        ),
-        Field('birth_quarter'),
-        Field('birth_decade'),
-        Field('birth_year'),
-        Field('serial_no'),
-        Field('block'),
-        css_id = 'form-row-div',
-        css_class='form-inline form-inline-nowrap',
-    )
-)
+CRISPY_FORM_1970 = FORM_1970
 
 
-# CRISPY_FORM_1970 = Layout(
-#     Div(    
-#     Div(
-#         Div('last_name',
-#             Div(
-#                 Div('first_name', css_class='col-lg-8 col-md-8 col-sm-8'),
-#                 Div('middle_init', css_class='col-lg-4 col-md-4 col-sm-4'),
-#                 css_class='row'
-#             ), 
-#             css_class='col-lg-3 col-md-3 col-sm-3'
-#         ),
-#         Div('relp_1970', css_class='col-lg-2 col-md-2 col-sm-2'),
-#         Div('sex', css_class='col-lg-1 col-md-1 col-sm-1'),
-#         Div(InlineRadios('race_1970'), css_class='col-lg-2 col-md-2 col-sm-2'),
-#         Div(
-#             Div('exact_birth_month', css_class='row'),
-#             Div('exact_birth_year', css_class='row'),
-#             Div('age', css_class='row'),
-#             css_class='col-lg-1 col-md-1 col-sm-1'
-#         ),
-#         Div('birth_quarter', css_class='col-lg-1 col-md-1 col-sm-1'),
-#         Div('birth_decade', css_class='col-lg-1 col-md-1 col-sm-1'),
-#         Div('birth_year', css_class='col-lg-1 col-md-1 col-sm-1'),
-#         css_class='row'
-#     ),
-#     Div(
-#         Div('serial_no', css_class='col-lg-1 col-md-1 col-sm-1'),
-#         Div('block', css_class='col-lg-1 col-md-1 col-sm-1'),
-#         css_class='row'
-#     ),
-#     css_class='table-bordered'
-#     )
-# )
+CRISPY_FORM_1980 = FORM_1980
 
+
+CRISPY_FORM_1990 = FORM_1990
 
 
 DEV_FORM_DICT = {
     1960: CRISPY_FORM_1960,
-    1970: CRISPY_FORM_1970
+    1970: CRISPY_FORM_1970,
+    1980: CRISPY_FORM_1980,
+    1990: CRISPY_FORM_1990,
 }
