e60a6ee792cda054eccf618d6afb0542cde8763b
Christian Moscardi <christian.l.moscardi@census.gov>
2021-11-23 17:12:00 -0500

    add restore command
    smaller changes;
    - reorder .pgpass args per suggestion

--- manage_backups.py
+++ manage_backups.py
@@ -2,6 +2,7 @@ import argparse
 from contextlib import contextmanager
 import os
 import pathlib
+import sys
 
 import subprocess
 import time
@@ -35,12 +36,11 @@ def pgpass_context(db_settings):
     if os.path.exists(PGPASS_PATH):
         raise KeyError("the .pgpass file exists, move before running this.")
 
-    pgpass_str = "{host}:{port}:{name}:{user}:{password}"\
-        .format(user=db_settings["USER"],
-                password=db_settings["PASSWORD"],
-                host=db_settings["HOST"],
+    pgpass_str = "{host}:{port}:*:{user}:{password}\n"\
+        .format(host=db_settings["HOST"],
                 port=db_settings["PORT"],
-                name=db_settings["NAME"])
+                user=db_settings["USER"],
+                password=db_settings["PASSWORD"])
 
     with open(PGPASS_PATH, "w+") as pgpass_f:
         pgpass_f.write(pgpass_str)
@@ -80,7 +80,7 @@ def run_backup(db_settings):
         subprocess.run(command, shell=True, stdout=out_f, env=env)
 
 
-def restore_backup(backup_path, new_db_name):
+def restore_backup(backup_path, db_settings, new_db_name):
     """
     Restores backup from specified file to a new database.
     Uses the user / account info from django config.
@@ -90,14 +90,33 @@ def restore_backup(backup_path, new_db_name):
        2. Restore backup to new database.
        3. Point production to new database.
     """
-    pass
+
+    # db is because otherwise the .pgpass file doesnt work
+    create_command = "psql -d {db} -U {user} -c 'CREATE DATABASE {name} WITH OWNER {user};'"\
+                         .format(db=db_settings["NAME"], user=db_settings["USER"], name=new_db_name)
+    restore_command = "psql -U {user} -d {new_db_name} -f {backup_path}"\
+                         .format(user=db_settings["USER"],
+                                 new_db_name=new_db_name,
+                                 backup_path=backup_path)
+    env = os.environ.copy()
+    env["PGPASSFILE"] = PGPASS_PATH
+    res = subprocess.run(create_command, shell=True, env=env)
+    if res.returncode != 0:
+        print("Error with DB creation command. The database may exist. Check command line output for messages from psql.")
+        sys.exit(1)
+
+    subprocess.run(restore_command, shell=True, env=env)
 
 
 commands = ["run_backup", "restore_backup"]
-helptext = 'Which command to run. Options: {}'.format(", ".join(commands))
+command_helptext = 'Which command to run. Options: {}'.format(", ".join(commands))
+new_db_helptext = 'Name of new database. Required if running restore_backup'
+backup_path_helptext = 'Location of backup file. Should be a .sql file. Required if running restore_backup'
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Do backup stuff.')
-    parser.add_argument('command', help=helptext, choices=commands)
+    parser.add_argument('command', help=command_helptext, choices=commands)
+    parser.add_argument('backup_path', nargs="?", help=backup_path_helptext)
+    parser.add_argument('new_db_name', nargs="?", help=new_db_helptext)
     args = parser.parse_args()
 
     db_settings = settings.DATABASES['default']
@@ -105,4 +124,6 @@ if __name__ == "__main__":
         if args.command == 'run_backup':
             run_backup(db_settings)
         elif args.command == 'restore_backup':
-            restore_backup()
+            if args.new_db_name is None or args.backup_path is None:
+                parser.error("new_db_name and backup_path required to restore backup")
+            restore_backup(args.backup_path, db_settings, args.new_db_name)
