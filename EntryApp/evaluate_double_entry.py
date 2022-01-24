27c40e46ded6215be8b14b9715ba18df8205dee3
murra394 <cecile.m.murray@census.gov>
2022-01-24 14:30:20 -0500

    double entry eval: reel check method works

--- EntryApp/evaluate_double_entry.py
+++ EntryApp/evaluate_double_entry.py
@@ -3,7 +3,6 @@
 #===============================================================#
 
 import pandas as pd
-import psycopg2 as pg
 
 from EntryApp.models import Breaker
 from EntryApp.models import Image
@@ -18,13 +17,13 @@ def test_dbl_records(model, **kwargs):
     return len(model.objects.filter(**kwargs)) == 2
 
 
-def compute_match(model, **kwargs):
+def compute_match(queryset, **kwargs):
     '''
     Check values in double-entered records: % match and mismatched fields 
     CURRENTLY ASSUMES ONLY TWO ENTRY VALUES, SO CHECK FOR THAT FIRST
 
     Takes:
-    - Model (e.g. Image, Sheet, Breaker)
+    - queryset of data model objects
     - keyword args for filtering
     Returns:
     - # values that match
@@ -32,22 +31,131 @@ def compute_match(model, **kwargs):
     - list of mismatched fields
     '''
 
-    entries = model.objects.filter(**kwargs).values()
+    # filter more if desired
+    entries = queryset.filter(**kwargs).values()
 
-    matches = 0
+    num_matches = 0
     mismatch = []
     for k in entries[0].keys():
 
         # skip certain fields we wouldn't expect to match
-        if k is in ['id', 'jbid', 'create_date', 'last_modified',]:
+        fields_to_skip = ['id', 'jbid', 'create_date', 'last_modified',]
+        if k in fields_to_skip:
             continue
 
         elif entries[0][k] == entries[1][k]:
-            matches += 1
+            num_matches += 1
         
         else:
             mismatch.append(k)
 
-    return matches, len(entries[0].keys()), mismatch
+    num_fields_checked = len(entries[0].keys()) - len(fields_to_skip)
+
+    return matches, num_fields_checked, mismatch
 
 
+def check_matches_in_reel(reel):
+    '''
+    Walks through a double-entered reel checking each image file for match
+
+    Takes:
+    - Reel object
+    Returns:
+    - data frame with results for each image file
+    '''
+
+    # get all the image files in the reel
+    image_file_qs = ImageFile.objects.filter(img_reel_id=reel.id)
+
+    # initialize results dict
+    results = {
+        'image_file': [],
+        'keyer_one': [],
+        'keyer_two': [],
+        'num_matches': [],
+        'num_fields': [],
+        'mismatch': [],
+    }
+
+    # loop through to do comparison
+    for image_file in image_file_qs:
+
+        results['image_file'].append(image_file.img_fxile_name)
+
+        # get images associated with this image file
+        image_qs = Image.objects.filter(image_file = image_file)
+        image_qs = image_qs.exclude(jbid='jbid123')
+
+        # skip if we don't have exactly two entries 
+        if len(image_qs) != 2:
+            continue
+
+        # get keyer info - TODO this seems suboptimal as written
+        results['keyer_one'].append(image_qs[0].jbid)
+        results['keyer_two'].append(image_qs[1].jbid)
+
+        # check matches
+        num_matches, num_fields, mismatch = compute_match(image_qs)
+        results['num_matches'].append(num_matches)
+        results['num_fields'].append(num_fields)
+        results['mismatch'].append(mismatch)
+
+    return pd.DataFrame.from_dict(results)
+
+
+def check_matches_in_sheet_records(reel):
+    '''
+    Walks through a double-entered sheet in a reel checking each record for match
+
+    Takes:
+    - reel object
+    Returns:
+    - data frame with results for each record
+    '''
+
+    # get all the image files in the reel
+    image_file_qs = ImageFile.objects.filter(img_reel_id=reel.id)
+
+    # initialize results dict
+    results = {
+        'image_file': [],
+        'image_id': [],
+        'sheet_id': [],
+        'record_id': [],
+        'keyer_one': [],
+        'keyer_two': [],
+        'num_matches': [],
+        'num_fields': [],
+        'mismatch': [],
+    }
+
+    # loop through to do comparison
+    for image_file in image_file_qs:
+
+        results['image_file'].append(image_file.img_file_name)
+
+        # get records associated with this image file
+        image_qs = Image.objects.filter(image_file = image_file)
+        image_qs = image_qs.filter(image_type = "sheet")
+        image_qs = image_qs.exclude(jbid = 'jbid123')
+
+        for image in image_qs:
+            
+            sheet_qs = Sheet.objects.filter(img = image)
+
+
+        # skip if we don't have exactly two entries 
+        if len(image_qs) != 2:
+            continue
+
+        # get keyer info - TODO this seems suboptimal as written
+        results['keyer_one'].append(image_qs[0].jbid)
+        results['keyer_two'].append(image_qs[1].jbid)
+
+        # check matches
+        num_matches, num_fields, mismatch = compute_match(image_qs)
+        results['num_matches'].append(num_matches)
+        results['num_fields'].append(num_fields)
+        results['mismatch'].append(mismatch)
+
+    return pd.DataFrame.from_dict(results)
