ea518d9c609ac05ea6490df150af5a60abb29fca
murra394 <cecile.m.murray@census.gov>
2022-05-11 16:01:55 -0400

    update start script for document root and url alias

--- start_app.sh
+++ start_app.sh
@@ -8,17 +8,18 @@ source /apps/user/${USER}/miniconda3/bin/activate /apps/user/${USER}/conda_envs/
 if [ $APP_DIR = 'dcdl_data_entry' ]
 then
     cd /apps/django/dcdl_data_entry
-    python manage.py runmodwsgi --port 8000 --server-root=/tmp/httpd_production
+    python manage.py runmodwsgi --port 8000 --server-root=/tmp/httpd_production --url-alias /images /data/storage/images/ --document-root /data/storage/images
+
 
 elif [ $APP_DIR = 'dcdl_train' ]
 then
     cd /apps/django/dcdl_train
-    python manage.py runmodwsgi --port 7000 --server-root=/tmp/httpd_training --url-alias /images /data/storage/images/training_images/
+    python manage.py runmodwsgi --port 7000 --server-root=/tmp/httpd_training --url-alias /images /data/storage/images/training_images/ --document-root /data/storage/images/training_images
 
 elif [ $APP_DIR = 'dcdl_test' ]
 then
     cd /apps/django/dcdl_test
-    python manage.py runmodwsgi --port 7002  --document-root /data/storage/images/test_images/ --server-root=/tmp/httpd_test
+    python manage.py runmodwsgi --port 7002  --document-root /data/storage/images/test_images/ --server-root=/tmp/httpd_test --url-alias /images/ /data/storage/images/test_images
 
 fi
 
